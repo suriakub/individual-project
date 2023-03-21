d6cb8cb4470b357d115424ace1a35c0beb094235
Jakub Suriak <2482569s@student.gla.ac.uk>
2023-03-21 21:56:38 +0000

    feat: added a label for the color picker

--- stable-frontend/src/components/ColorPicker.tsx
+++ stable-frontend/src/components/ColorPicker.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import reactCSS from 'reactcss';
-import { ChromePicker, ColorResult} from 'react-color';
+import { ChromePicker, ColorResult } from 'react-color';
 
-export default function ColorPicker (props: {color: string, setColor: (color: string) => void}) {
+export default function ColorPicker(props: {
+  color: string;
+  setColor: (color: string) => void;
+}) {
   const [display, setDisplay] = useState(false);
 
   const handleClick = () => {
@@ -17,49 +20,50 @@ export default function ColorPicker (props: {color: string, setColor: (color: st
     props.setColor(color.hex);
   };
 
-    const styles = reactCSS({
-      default: {
-        color: {
-          width: '36px',
-          height: '14px',
-          borderRadius: '2px',
-          background: props.color,
-        },
-        swatch: {
-          padding: '5px',
-          background: '#fff',
-          borderRadius: '1px',
-          boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
-          display: 'inline-block',
-          cursor: 'pointer',
-        },
-        popover: {
-          position: 'absolute',
-          zIndex: '30',
-        },
-        cover: {
-          top: '0px',
-          right: '0px',
-          bottom: '0px',
-          left: '0px',
-        },
+  const styles = reactCSS({
+    default: {
+      color: {
+        width: '36px',
+        height: '14px',
+        borderRadius: '2px',
+        background: props.color
       },
-    });
+      swatch: {
+        padding: '5px',
+        background: '#fff',
+        borderRadius: '1px',
+        boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
+        display: 'inline-block',
+        cursor: 'pointer'
+      },
+      popover: {
+        position: 'absolute',
+        zIndex: '30'
+      },
+      cover: {
+        top: '0px',
+        right: '0px',
+        bottom: '0px',
+        left: '0px'
+      }
+    }
+  });
 
-    return (
-      <div>
-        <div style={styles.swatch} onClick={handleClick}>
-          <div style={styles.color} />
-        </div>
-        {display ? (
-          <div style={{...styles.popover, position: 'absolute'}}>
-            <div style={{...styles.cover, position: 'fixed' }} onClick={handleClose} />
-            <ChromePicker
-              color={props.color}
-              onChange={handleChange}
-            />
-          </div>
-        ) : null}
+  return (
+    <div>
+      Color:
+      <div style={styles.swatch} onClick={handleClick}>
+        <div style={styles.color} />
       </div>
-    );
+      {display ? (
+        <div style={{ ...styles.popover, position: 'absolute' }}>
+          <div
+            style={{ ...styles.cover, position: 'fixed' }}
+            onClick={handleClose}
+          />
+          <ChromePicker color={props.color} onChange={handleChange} />
+        </div>
+      ) : null}
+    </div>
+  );
 }
